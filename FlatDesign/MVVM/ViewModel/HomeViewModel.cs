46744f799a1a2e260516d6e28f1c64e4b484ceb4
UchiTesing <uchitesting57@gmail.com>
2022-03-17 17:43:35 +0100

    Integrating Home view in the main view

---
+++ FlatDesign/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FlatDesign.Core;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlatDesign.MVVM.ViewModel
+{
+    internal class HomeViewModel : ObservableObject
+    {
+        public HomeViewModel()
+        {
+
+        }
+    }
+}
---
+++ FlatDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using FlatDesign.Core;
+using FlatDesign.MVVM.View;
+
+namespace FlatDesign.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        private object _currentView;
+
+        public HomeViewModel HomeVm { get; set; }
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() {
+            HomeVm = new HomeViewModel();
+            CurrentView = new HomeViewModel();
+        }
+    }
+}
